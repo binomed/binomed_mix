b6f241afc91a5071e4811266ade30fb97d993e6e
jefbinomed <jean.francois.garreau@gmail.com>
2025-03-07 18:04:21 +0100

    first comit

---
+++ docs/playlist.js
@@ -0,0 +1,13 @@
+var dadoonetPlayList = [
+    {
+        title: '2025-02-18 - House Mix',
+        file: 'https://bafybeicbmp5wohe5awgewd6wjpaosbaoqpl5vichvcfydeyncvn3lv6a5e.ipfs.w3s.link/2025-02-15-house-mix.mp3',
+        file: 'https://bafybeicbmp5wohe5awgewd6wjpaosbaoqpl5vichvcfydeyncvn3lv6a5e.ipfs.w3s.link/2025-02-15-house-mix.mp3',
+        image: 'img/binomed_sun_flower.png',
+    },
+    {
+        title: 'Elastic DecemberFest 2024',
+        file: 'https://storage.googleapis.com/djdadoo/2024-12-05-ElasticDecemberFest.mp3',
+        image: 'img/binomed_sun_flower.png',
+    },
+];
