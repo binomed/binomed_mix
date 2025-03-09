d90b70b25ba39252afa5f7fa850e7fb78708a519
jefbinomed <jean.francois.garreau@gmail.com>
2025-03-09 21:13:37 +0100

    new mixs

--- docs/playlist.js
+++ docs/playlist.js
@@ -1,13 +1,26 @@
 var dadoonetPlayList = [
     {
-        title: '2025-02-18 - House Mix',
+        title: '2025-03-09 - House Disco Mix',
+        file: 'https://bafybeif7gd57vqnwb4tkbubb7ao7f46athetzjhmdwig3tts5sqremfk34.ipfs.w3s.link/2025-03-09-house-disco-mix.mp3',
+        file: 'https://bafybeif7gd57vqnwb4tkbubb7ao7f46athetzjhmdwig3tts5sqremfk34.ipfs.w3s.link/2025-03-09-house-disco-mix.mp3',
+        image: 'img/binomed_sun_flower.png',
+    },
+    {
+        title: '2025-02-23 - House Mix',
+        file: 'https://bafybeicdeaa4iawvlqvqrzkqkr37hchqmvbltjddf42jvznby7kfopl56u.ipfs.w3s.link/2025-02-23-house-mix.mp3',
+        file: 'https://bafybeicdeaa4iawvlqvqrzkqkr37hchqmvbltjddf42jvznby7kfopl56u.ipfs.w3s.link/2025-02-23-house-mix.mp3',
+        image: 'img/binomed_sun_flower.png',
+    },
+    {
+        title: '2025-02-15 - House Mix',
         file: 'https://bafybeicbmp5wohe5awgewd6wjpaosbaoqpl5vichvcfydeyncvn3lv6a5e.ipfs.w3s.link/2025-02-15-house-mix.mp3',
         file: 'https://bafybeicbmp5wohe5awgewd6wjpaosbaoqpl5vichvcfydeyncvn3lv6a5e.ipfs.w3s.link/2025-02-15-house-mix.mp3',
         image: 'img/binomed_sun_flower.png',
     },
     {
-        title: 'Elastic DecemberFest 2024',
-        file: 'https://storage.googleapis.com/djdadoo/2024-12-05-ElasticDecemberFest.mp3',
+        title: 'Total Mix 2',
+        file: 'https://bafybeiefl6kci5quijhojiwj23o4vtfmlliae6ranod65esb36vvvpfom4.ipfs.w3s.link/total%20mix2.mp3',
+        file: 'https://bafybeiefl6kci5quijhojiwj23o4vtfmlliae6ranod65esb36vvvpfom4.ipfs.w3s.link/total%20mix2.mp3',
         image: 'img/binomed_sun_flower.png',
     },
 ];
