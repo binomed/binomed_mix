79db38287d65234cd8b6ec3807f27122703b1cef
jefbinomed <jean.francois.garreau@gmail.com>
2025-05-09 18:30:17 +0200

    mix du 9 mai

--- docs/playlist.js
+++ docs/playlist.js
@@ -1,5 +1,10 @@
 var binomedPlayList = [
     {
+        title: '2025-05-09 - House Mix',
+        file: 'https://storage.googleapis.com/binomed-mix/2025-05-09-House-Mix.mp3',
+        image: 'img/binomed_sun_flower.png',
+    },
+    {
         title: '2025-05-03 - House Mix',
         file: 'https://storage.googleapis.com/binomed-mix/2025-05-03-House-Mix.mp3',
         image: 'img/binomed_sun_flower.png',
