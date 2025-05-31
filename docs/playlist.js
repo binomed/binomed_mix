2cf816e07048b44539f9aa75eaf7c63fb5a94f67
jefbinomed <jean.francois.garreau@gmail.com>
2025-05-31 21:28:03 +0200

    Mix du 31 mai

--- docs/playlist.js
+++ docs/playlist.js
@@ -1,5 +1,10 @@
 var binomedPlayList = [
     {
+        title: '2025-05-31 - House Disco Mix',
+        file: 'https://storage.googleapis.com/binomed-mix/2025-05-31-House-Disco-Mix.mp3',
+        image: 'img/binomed_sun_flower.png',
+    },
+    {
         title: '2025-05-25 - Birthday Mix',
         file: 'https://storage.googleapis.com/binomed-mix/2025-05-25-Birthday-mix.mp3',
         image: 'img/binomed_sun_flower.png',
