5baf10ba936a646eeddc5998e5883f57e1241fa2
jefbinomed <jean.francois.garreau@gmail.com>
2025-11-30 20:11:05 +0100

    mix du 30 novembre

--- docs/playlist.js
+++ docs/playlist.js
@@ -1,5 +1,10 @@
 var binomedPlayList = [
     {
+        title: '2025-11-30 - Disco Mix',
+        file: 'https://storage.googleapis.com/binomed-mix/2025-11-30-Disco-Mix.mp3',
+        image: 'img/binomed_sun_flower.png',
+    },
+    {
         title: '2025-11-23 - House Mix',
         file: 'https://storage.googleapis.com/binomed-mix/2025-11-23-House-Mix.mp3',
         image: 'img/binomed_sun_flower.png',
