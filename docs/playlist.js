48738b2c1478b2ab9748de14331c38e96a8da7f0
jefbinomed <jean.francois.garreau@gmail.com>
2026-02-22 17:29:42 +0100

    mix du 22 février

--- docs/playlist.js
+++ docs/playlist.js
@@ -1,5 +1,10 @@
 var binomedPlayList = [
     {
+        title: '2026-02-22 - House Fresh Mix',
+        file: 'https://storage.googleapis.com/binomed-mix/2026-02-22-House-Fresh-Mix.mp3',
+        image: 'img/binomed_sun_flower.png',
+    },
+    {
         title: '2026-02-16 - House Mix',
         file: 'https://storage.googleapis.com/binomed-mix/2026-02-16-House-Mix.mp3',
         image: 'img/binomed_sun_flower.png',
