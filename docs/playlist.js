99969d7192be04443753cc77e4e9202f0bfc2258
jefbinomed <jean.francois.garreau@gmail.com>
2026-04-20 19:39:50 +0200

    ajout mix du 20 avril

--- docs/playlist.js
+++ docs/playlist.js
@@ -1,5 +1,10 @@
 var binomedPlayList = [
     {
+        title: '2026-04-20 - House Mix',
+        file: 'https://storage.googleapis.com/binomed-mix/2026-04-20-House-Mix.mp3',
+        image: 'img/binomed_sun_flower.png',
+    },
+    {
         title: '2026-04-12 - House Mix',
         file: 'https://storage.googleapis.com/binomed-mix/2026-04-12-House-Mix.mp3',
         image: 'img/binomed_sun_flower.png',
